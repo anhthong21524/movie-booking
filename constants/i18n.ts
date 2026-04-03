923e786a96b580c2604c83392a1176f10bf844ea
ldthong <anhthong21524@gmail.com>
2026-04-03 22:51:29 +0700

    Set Vietnamese as default locale

--- constants/i18n.ts
+++ constants/i18n.ts
@@ -2,7 +2,7 @@ export const SUPPORTED_LOCALES = ['en', 'vi'] as const
 
 export type AppLocale = (typeof SUPPORTED_LOCALES)[number]
 
-export const DEFAULT_LOCALE: AppLocale = 'en'
+export const DEFAULT_LOCALE: AppLocale = 'vi'
 
 export const messages = {
   en: {
